4e8366457f74ed9af17b6494144cf3c1bf4aa594
Man Yee Cheung <manyee.cheung@ao.com>
2019-07-30 15:03:22 +0100

    fixed test 6

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,31 +9,31 @@ import { greet } from '../index';
 
 describe('Greet', () => {
 	it('should return a string "Hello, Bob" when given "Bob"', () => {
-		const greeting = greet("Bob");
-		expect(greeting).toBe("Hello, Bob");
-	})
+		const greeting = greet('Bob');
+		expect(greeting).toBe('Hello, Bob');
+	});
 
 	it('should return "Hello, my friend." when given null', () => {
 		const greeting = greet(null);
-		expect(greeting).toBe("Hello, my friend.");
+		expect(greeting).toBe('Hello, my friend.');
 	});
 
 	it('should return "HELLO JERRY!." when given "JERRY"', () => {
-		const greeting = greet("JERRY");
-		expect(greeting).toBe("HELLO JERRY!");
+		const greeting = greet('JERRY');
+		expect(greeting).toBe('HELLO JERRY!');
 	});
 	it('should return "Hello, Jill and Jane" when given an array of ["Jill", "Jane"]', () => {
-		const greeting = greet(["Jill", "Jane"]);
-		expect(greeting).toBe("Hello, Jill and Jane.");
+		const greeting = greet(['Jill', 'Jane']);
+		expect(greeting).toBe('Hello, Jill and Jane.');
 	});
 
 	it('should return "Hello, [name], [name], and [name]" when given an array of names', () => {
-		const greeting = greet(["Amy", "Brian", "Charlotte"]);
-		expect(greeting).toBe("Hello, Amy, Brian, and Charlotte.");
+		const greeting = greet(['Amy', 'Brian', 'Charlotte']);
+		expect(greeting).toBe('Hello, Amy, Brian, and Charlotte.');
 	});
 
 	it('should return "Hello, [name], [name], AND [NAME]! when given an array of names', () => {
-		const greeting = greet(["Amy", "BRIAN", "Charlotte"]);
-		expect(greeting).toBe("Hello, Amy, Charlotte, AND BRIAN!");
+		const greeting = greet(['Amy', 'BRIAN', 'Charlotte']);
+		expect(greeting).toBe('Hello, Amy and Charlotte. AND HELLO BRIAN!');
 	});
-})
+});
--- index.js
+++ index.js
@@ -6,18 +6,53 @@ function isAllUppercase(myString) {
 	return myString === myString.toUpperCase();
 }
 
+function namesByCase(arr) {
+	return arr.reduce(
+		(acc, name) => {
+			if (isAllUppercase(name)) {
+				acc.upperCaseNames.push(name);
+			} else {
+				acc.lowerCaseNames.push(name);
+			}
+			return acc;
+		},
+		{ upperCaseNames: [], lowerCaseNames: [] }
+	);
+}
+
+function greetLowerCaseNames(names) {
+	if (!names.length) {
+		return '';
+	}
+	if (names.length > 2) {
+		const lastElement = names.pop();
+		return `Hello, ${names.join(', ')}, and ${lastElement}.`;
+	}
+	return `Hello, ${names.join(' and ')}.`;
+}
+
+// function greetUpperCaseNames(names) {
+// 	if(!names.length) {
+// 		return ''
+// 	}
+// 	ret
+// }
+
 export function greet(name) {
-	if(name === null) {
-		return 'Hello, my friend.'
+	if (name === null) {
+		return 'Hello, my friend.';
 	}
 	if (Array.isArray(name)) {
-		if(name.length > 2) {
-			const lastElement = name.pop()
-			return `Hello, ${name.join(', ')}, and ${lastElement}.`
+		const { upperCaseNames, lowerCaseNames } = namesByCase(name);
+		const lowerCaseGreetings = greetLowerCaseNames(lowerCaseNames);
+		if (upperCaseNames.length && lowerCaseNames.length) {
+			return `${lowerCaseGreetings} AND HELLO ${upperCaseNames}!`;
 		}
-		return `Hello, ${name.join(' and ')}.`;
+		if (lowerCaseNames.length) {
+			return lowerCaseGreetings;
+		}
 	}
-	if(isAllUppercase(name)) {
+	if (isAllUppercase(name)) {
 		return `HELLO ${name}!`;
 	}
 	return `Hello, ${name}`;
